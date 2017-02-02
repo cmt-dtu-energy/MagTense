e40ee9a81e8a00b0aaac3baa462ff60f0897a934
Kaspar Kirstein Nielsen <kaki@dtu.dk>
2017-02-02 12:40:15 +0100

    Tested various things

--- MagStatPrismMEX/MagStatPrismMEX/integral2_mex.f90
+++ MagStatPrismMEX/MagStatPrismMEX/integral2_mex.f90
@@ -7,19 +7,234 @@
       
       type modelStruct
       integer,dimension(3) :: inds
-      real*8 :: z0
+      real*8 :: z0,r0
       integer :: n,spacedim,intInd
       real*8,dimension(:,:),allocatable :: dims,pos,M,rotAngles
       real*8,dimension(:,:,:),allocatable :: rotMat,rotMatInv
+      integer*8 :: cylPlane
+      integer*8,dimension(3) :: retForces
       endtype
-      
+      integer*8,parameter :: phi_z_plane=1,r_phi_plane=2
       type(modelStruct) :: model
       
       real*8,parameter :: mu0=4*pi*1e-7
             
        procedure (func), pointer :: f_tens_ptr => null()
+       
+       procedure (func), pointer :: f_tens_ptr_cyl1 => null()
+       procedure (func), pointer :: f_tens_ptr_cyl2 => null()
+       procedure (func), pointer :: f_tens_ptr_cyl3 => null()
       
       contains
+      !::
+      !::Integrates Maxwell's stress tensor over a cylindrical volume
+      !::defines from rMax and zPos (zPos(1) = zMin and zPos(2) = zMax)
+      !::eps_abs defines the minimum absolute error
+      !::eps_rel defines the minimum relative error
+      !::F is the resulting force
+      !::ier is an integer describing the error state 
+      !::of the integration (0 = no error)
+      !:: abserr_tot is the total absolute error
+      !::neval is the number of function evaluations used by the integrator
+      subroutine cylIntegral( rMax, zPos, eps_abs, eps_rel, 
+     +                       F, ier,abserr_tot, neval)
+      real*8,intent(in) :: rMax,eps_abs,eps_rel
+      real*8,dimension(2),intent(in) :: zPos
+      real*8,dimension(3),intent(inout) :: F
+      integer*8,intent(inout) :: ier,neval
+      real*8,intent(inout) :: abserr_tot
+      integer*8 :: j
+      real*8 :: res,abserr
+      real*8,parameter :: zero=0.0
+       !::Reset input array
+      F(:) = 0.
+      
+      !set to a large number
+      abserr_tot = 1e9
+      neval = 0
+      !::Get the rotation matrices
+      call findRotationMatrices( model%rotAngles, model%n, 
+     +                            model%rotMat, model%rotMatInv)
+      
+     
+       do j=3,3
+          
+      if ( j .eq. 1 ) then        
+        !::The r-component of the force
+        f_tens_ptr_cyl1 => F_int_diag_cyl
+        f_tens_ptr_cyl2 => F_int_off_diag_cyl
+        f_tens_ptr_cyl3 => F_int_off_diag_cyl
+        model%intInd = 3
+        model%inds(3) = 1
+      elseif ( j .eq. 2 ) then
+        !:: the phi-component of the force
+        f_tens_ptr_cyl1 => F_int_off_diag_cyl
+        f_tens_ptr_cyl2 => F_int_off_diag_cyl
+        f_tens_ptr_cyl3 => F_int_off_diag_cyl        
+        model%inds(3) = 2
+        model%intInd = 1
+      elseif ( j .eq. 3 ) then
+        !:: the z-component of the force
+        f_tens_ptr_cyl1 => F_int_off_diag_cyl
+        f_tens_ptr_cyl2 => F_int_diag_cyl
+        f_tens_ptr_cyl3 => F_int_diag_cyl
+        model%intInd = 1
+        model%inds(3) = 3
+      endif    
+          
+      !::The phi-z plane
+      model%cylPlane = phi_z_plane
+      model%r0 = rMax
+      !call integral2( f_tens_ptr_cyl1, zero, 2*pi, 
+      !+                                zPos(1), zPos(2), 
+      !+                                eps_abs, eps_rel,
+      !+                                res, abserr, neval, ier )
+      !    if ( abserr .lt. abserr_tot ) then
+      !        abserr_tot = abserr
+      !    endif      
+      !    if ( ier .gt. 0 ) then
+      !        exit
+      !    endif
+      !F(j) = res
+      !::Lower r-phi plane
+      model%cylPlane = r_phi_plane
+      model%z0 = zPos(1)
+      
+      if ( j .eq. 2 ) then
+          model%intInd = 3
+      endif      
+      call integral2( f_tens_ptr_cyl2, zero, rMax, 
+     +                                zero, 2*pi, 
+     +                                eps_abs, eps_rel,
+     +                                res, abserr, neval, ier )
+          if ( abserr .lt. abserr_tot ) then
+              abserr_tot = abserr
+          endif      
+          if ( ier .gt. 0 ) then
+              exit
+          endif
+      F(j) = F(j) - res
+      
+      !::Upper plane
+      model%cylPlane = r_phi_plane
+      model%z0 = zPos(2)
+      call integral2( f_tens_ptr_cyl3, zero, rMax, 
+     +                                zero, 2*pi, 
+     +                                eps_abs, eps_rel,
+     +                                res, abserr, neval, ier )
+          if ( abserr .lt. abserr_tot ) then
+              abserr_tot = abserr
+          endif      
+          if ( ier .gt. 0 ) then
+              exit
+          endif
+      F(j) = F(j) + res
+      enddo
+      end subroutine cylIntegral
+      
+      !::Returns the integrand for the off-diagonal part of the Maxwell stress
+      !::tensor dotted with the surface area
+      function F_int_off_diag_cyl( v1, v2 )
+      real*8 :: F_int_off_diag_cyl
+      real*8,intent(in) :: v1,v2
+      real*8 :: x,y,z,r,phi          
+      real*8,dimension(3) :: solPts,B,Hsol
+      
+      Hsol(:) = 0
+      
+      call car2cyl( v1, v2, r, phi, x, y, z )
+      
+      !::setup the points where we wish to get the solution
+      solPts(1) = x
+      solPts(2) = y
+      solPts(3) = z
+
+      !::call the subroutine to find the solution at the specified points
+      call getSolution( solPts, 1, model%M, model%dims, 
+     +                  model%pos, model%n, model%spacedim,
+     +                  Hsol, model%rotMat, model%rotMatInv )
+      
+      !::Convert to cylindrical coordiantes                                  
+      call getCylVec( phi, Hsol, B )
+    
+       F_int_off_diag_cyl = 
+     + 1. /mu0 * r * B(model%intInd) * B(model%inds(3))
+    
+       return
+
+      end function F_int_off_diag_cyl
+      
+      !::Returns the integrand of the Maxwell stress tensor along the diagonal over
+      !::the given surface area. 
+      !::            
+      function F_int_diag_cyl( v1, v2 )
+      real*8 :: F_int_diag_cyl
+      real*8,intent(in) :: v1,v2
+      real*8 :: x,y,z,r,phi      
+      real*8,dimension(3) :: solPts,B,Hsol
+      real*8 :: Bnorm
+            
+      Hsol(:) = 0
+       
+      call car2cyl( v1, v2, r, phi, x, y, z )
+      
+      solPts(model%inds(1)) = x
+      solPts(model%inds(2)) = y
+      solPts(model%inds(3)) = z
+
+    
+      call getSolution( solPts, 1, model%M, model%dims, 
+     +                  model%pos, model%n, model%spacedim,
+     +                  Hsol, model%rotMat, model%rotMatInv )
+	
+      call getCylVec( phi, Hsol, B )
+      Bnorm = sqrt( B(1)**2 + B(2)**2 + B(3)**2 )
+                                 
+      F_int_diag_cyl = 1./mu0 * 
+     +                   ( B(model%inds(3))**2 - 0.5 * Bnorm**2 )
+    
+        
+      end function F_int_diag_cyl
+      !::
+      !::Converts v1 and v2 to cylindrical and carthesian coordinates
+      !::based on which plane is currently selected in the model
+      !::
+      subroutine car2cyl( v1, v2, r, phi, x, y, z )
+      real*8,intent(in) :: v1,v2
+      real*8,intent(inout) :: r, phi, x, y, z
+      !::Make coordinate transformation
+      if ( model%cylPlane .eq. phi_z_plane ) then
+      !::Integrate over the phi-z plane, i.e. r = const
+      !::v1 is then assumed to be phi and v2 to be z
+          r = model%r0
+          phi = v1      
+          z = v2
+      else
+      !::Integrate over the r-phi plane, i.e. z = const
+      !::v1 is assumed to be r and v2 to be phi
+          r = v1
+          phi = v2      
+          z = model%z0
+      endif
+      x = r * cos( phi )
+      y = r * sin( phi )
+      
+      end subroutine car2cyl
+      
+      subroutine getCylVec( phi, Hsol, B )
+      real*8,intent(in) :: phi
+      real*8,dimension(3),intent(in) :: Hsol
+      real*8,dimension(3),intent(inout) :: B
+       !::Convert to cylindrical coordinates
+       B(1) = mu0 * (  cos(phi) * Hsol(1) + sin(phi) * Hsol(2) )
+       B(2) = mu0 * ( -sin(phi) * Hsol(1) + cos(phi) * Hsol(2) )
+       B(3) = mu0 * Hsol(3)
+      
+      end subroutine getCylVec
+      
+      
+      
+      
       !::
       !::Performs the box integral around the region specified 
       !::and over the Maxwell stress tensor using integral2 via quadpack
@@ -27,14 +242,15 @@
       !::yPos(2) defines min and max of the y range
       !::zPos(2) defines min and max of the z range
       !::eps_abs defines the minimum absolute error
-      !::eps_rel defines the minimum relative error
-      !::mdStr is a struct with data from the model run and information to be shared
-      !::between integration function calls      
+      !::eps_rel defines the minimum relative error      
+      !::F is the resulting force
+      !::ier is an integer describing the error state 
+      !::of the integration (0 = no error)
+      !:: abserr_tot is the total absolute error
+      !::neval is the number of function evaluations used by the integrator
       subroutine boxIntegral(xPos, yPos, zPos, eps_abs, eps_rel, 
      +                       F, ier,abserr_tot, neval)
-      !use model_glob
-      !USE MagPrism_Call
-      !USE Parameters_CALL
+      
       real*8,dimension(2),intent(in) :: xPos,yPos,zPos
       real*8,dimension(3),intent(inout) :: F
       real*8,intent(inout) :: abserr_tot
@@ -109,55 +325,57 @@
      +                            model%rotMat, model%rotMatInv)
       !::j loops over the three components
       do j=1,3
-      !:: i loops over the three surface pairs
-      do i=1,3
+        if ( model%retForces(j) .eq. 0 ) then
+          !:: i loops over the three surface pairs
+          do i=1,3
           
-          if ( i .eq. 1 ) then
-              !::The diagonal components are always the first surface pair
-              f_tens_ptr => F_int_diag
-          elseif ( i .gt. 1 ) then
-              !::And then comes the two surface pairs with off-diagonal components
-              f_tens_ptr => F_int_off_diag
-          endif    
-          !::Set the plane indices
-          model%inds(1:3) = indices(:,i,j)
-          !::set the index of the first integration function in the integrand
-          model%intInd = j
-          !::lower plane 
-           model%z0 = zp(i,1,j)
+              if ( i .eq. 1 ) then
+                  !::The diagonal components are always the first surface pair
+                  f_tens_ptr => F_int_diag
+              elseif ( i .gt. 1 ) then
+                  !::And then comes the two surface pairs with off-diagonal components
+                  f_tens_ptr => F_int_off_diag
+              endif    
+              !::Set the plane indices
+              model%inds(1:3) = indices(:,i,j)
+              !::set the index of the first integration function in the integrand
+              model%intInd = j
+              !::lower plane 
+               model%z0 = zp(i,1,j)
           
-          !::direction of the other xy plane
-          call integral2( f_tens_ptr, xp(i,1,j), xp(i,2,j), 
+              !::direction of the other xy plane
+              call integral2( f_tens_ptr, xp(i,1,j), xp(i,2,j), 
      +                                yp(i,1,j), yp(i,2,j), 
      +                                eps_abs, eps_rel,
      +                                res, abserr, neval, ier )
-          if ( abserr .lt. abserr_tot ) then
-              abserr_tot = abserr
-          endif      
-          if ( ier .gt. 0 ) then
-              exit
-          endif
+              if ( abserr .lt. abserr_tot ) then
+                  abserr_tot = abserr
+              endif      
+              if ( ier .gt. 0 ) then
+                  exit
+              endif
           
-          !::Note the sign accounts for the normal vector pointing the opposite
-          F(j) = F(j) - res
-          !::upper plane
-          model%z0 = zp(i,2,j)
+              !::Note the sign accounts for the normal vector pointing the opposite
+              F(j) = F(j) - res
+              !::upper plane
+              model%z0 = zp(i,2,j)
            
-          call integral2( f_tens_ptr, xp(i,1,j), xp(i,2,j), 
+              call integral2( f_tens_ptr, xp(i,1,j), xp(i,2,j), 
      +                                yp(i,1,j), yp(i,2,j), 
      +                                eps_abs, eps_rel,
      +                                res, abserr, neval, ier )
-          if ( abserr .lt. abserr_tot ) then
-              abserr_tot = abserr
-          endif      
-          if ( ier .gt. 0 ) then
-              exit
-          endif
+              if ( abserr .lt. abserr_tot ) then
+                  abserr_tot = abserr
+              endif      
+              if ( ier .gt. 0 ) then
+                  exit
+              endif
      
-          F(j) = F(j) + res
+              F(j) = F(j) + res
           
           
-      enddo
+          enddo
+          endif
       enddo
       
       end subroutine boxIntegral
@@ -241,21 +459,17 @@
 !     prhs(9) zlim (2), z bounds
 !     prhs(10) eps_abs, real*8
 !     prhs(11) eps_rel, real*8
+!     prhs(12) retForces (optional), integer [3,1] array with 0 indicates false and otherwise true
 
 !     plhs(1) Fout(3) output force vector [Fx,Fy,Fz]
+!     plhs(2) ier, integer returned
+!     plhs(3) max_err, real
+!     plhs(4) neval, integer, number of function evaluations
 
       subroutine mexFunction(nlhs, plhs, nrhs, prhs)
       use model_glob
 
       implicit none
-!      type modelStruct
-!      integer,dimension(3) :: inds
-!      real*8 :: z0
-!      integer :: n,spacedim
-!      real*8,dimension(:,:),allocatable :: dims,pos,M,rotAngles
-!      real*8,dimension(:,:,:),allocatable :: rotMat,rotMatInv
-!      endtype
-     
       
 !     mexFunction arguments:
       mwPointer plhs(*), prhs(*)
@@ -288,6 +502,7 @@
       integer*8 :: ier,neval
       real*8 :: maxerr_tot,eps_abs, eps_rel
       
+      
 	  maxerr_tot = 0
 	  !-----------------------------------------------------------------------	  	       
 	     open(11,file='version_integral2_mex.txt',
@@ -301,9 +516,9 @@
       
       !-----------------------------------------------------------------------
 !     Check for proper number of arguments. 
-      if(nrhs .ne. 11) then
+      if(nrhs .lt. 11) then
          call mexErrMsgIdAndTxt ('MATLAB:magStat_mex:nInput',
-     +                           '9 inputs are required.')
+     +                           'at least 11 inputs are required.')
       elseif(nlhs .gt. 4) then
          call mexErrMsgIdAndTxt ('MATLAB:magStat_mex:nOutput',
      +                           'Too many output arguments.')
@@ -342,7 +557,10 @@
      +                           'Input ten should be a double')      
       elseif ( .NOT. mxIsDouble(prhs(11)) ) then
           call mexErrMsgIdAndTxt ('MATLAB:Matlab_single_mex:DataType',
-     +                           'Input eleven should be a double')      
+     +                           'Input 11 should be a double')      
+      elseif (nrhs .ge. 12 .and. .NOT. mxIsInt32(prhs(12)) ) then
+          call mexErrMsgIdAndTxt ('MATLAB:Matlab_single_mex:DataType',
+     +                           'Input 12 should be an integer')
       endif
       
       !::Allocate input data
@@ -364,11 +582,19 @@
       
       call mxCopyPtrToReal8(mxGetPr(prhs(10)), eps_abs, 2 )
       call mxCopyPtrToReal8(mxGetPr(prhs(11)), eps_rel, 2 )
+      
+      if ( nrhs .ge. 12 ) then         
+          call mxCopyPtrToInteger4(mxGetPr(prhs(12)), model%retForces,3) 
+      else
+          model%retForces(:) = 1
+      endif
      
       !::Load the result back to Matlab
       call boxIntegral( xlim, ylim, zlim, eps_abs, eps_rel, Fout, 
      +                  ier, maxerr_tot, neval )
-      
+     
+      !call cylIntegral( xlim(1), zlim, eps_abs, eps_rel, Fout, 
+      !+                  ier, maxerr_tot, neval )
       ComplexFlag = 0;
       
       plhs(1) = mxCreateDoubleMatrix( 3, 1, ComplexFlag )
