338ae16c211f452dc711f92f8228af572ee721df
Kaspar Kirstein Nielsen <kaki@dtu.dk>
2017-12-19 20:41:07 +0100

    Moved I/O code from specialised .mex files to a fortran library for easier portability

--- MagStatPrismMEX/MagStatPrismMEX/getNFromCylTile_mex.f90
+++ MagStatPrismMEX/MagStatPrismMEX/getNFromCylTile_mex.f90
@@ -9,6 +9,7 @@
     !::plhs(1) is a [3,3,n] double array containing the output tensor field
       subroutine mexFunction(nlhs, plhs, nrhs, prhs)
       use MagStat2GetSolution
+      use MagTileIO
       implicit none
       
 !     mexFunction arguments:
@@ -17,35 +18,17 @@
 
 !     Function declarations:
       mwPointer mxGetPr
-      mwPointer mxCreateDoubleMatrix
       mwPointer mxCreateNumericArray
-      integer*4 mxIsNumeric
-      integer*4 mxIsDouble
-      integer*4 mxIsInt32
-      integer*4 mxIsStruct
-      integer*4 mxIsCell
-      integer*4 mxClassIDFromClassName
-    
-      mwPointer mxGetField
-      mwPointer mxGetFieldByNumber
-      
-      mwPointer mxGetM, mxGetN
-      mwSize mxGetNumberOfDimensions, sx
-      mwPointer mxGetDimensions
-      
+      integer*4 mxIsNumeric, mxIsDouble, mxIsInt32, mxIsStruct, mxClassIDFromClassName
+          
       integer*4 ComplexFlag,classid
 
 !     Pointers to input/output mxArrays:
-      mwPointer :: stPr
       real*8,dimension(:,:),allocatable :: pts,H
       real*8,dimension(:,:,:),allocatable :: N
-      real*8,dimension(3) :: rectDims
-      type(MagTile):: cylTile
-      mwPointer :: r0Ptr,theta0Ptr,z0Ptr,drPtr,dthetaPtr,dzPtr,MPtr,u_eaPtr,u_oa1Ptr,u_oa2Ptr,mur_eaPtr,mur_oaPtr,MremPtr
-      mwPointer :: tileTypePtr,offsetPtr,rotAnglesPtr,rectDimsPtr
+      type(MagTile),dimension(1):: cylTile
       integer*4 :: n_ele
-      mwSize,dimension(3) :: dims
-      mwIndex :: i
+      mwSize,dimension(3) :: dims      
       
 !     Check for proper number of arguments. 
       if( nrhs .ne. 3) then
@@ -66,46 +49,8 @@
       endif                  
             
       !::Copy the input parameters
-      sx = 1            
-      
-        r0Ptr = mxGetField(prhs(1),sx,'r0')
-        theta0Ptr = mxGetField(prhs(1),sx,'theta0')
-        z0Ptr = mxGetField(prhs(1),sx,'z0')
-        drPtr = mxGetField(prhs(1),sx,'dr')
-        dthetaPtr = mxGetField(prhs(1),sx,'dtheta')
-        dzPtr = mxGetField(prhs(1),sx,'dz')
-        MPtr = mxGetField(prhs(1),sx,'M')
-        u_eaPtr = mxGetField(prhs(1),sx,'u_ea')
-        u_oa1Ptr = mxGetField(prhs(1),sx,'u_oa1')
-        u_oa2Ptr = mxGetField(prhs(1),sx,'u_oa2')
-        mur_eaPtr = mxGetField(prhs(1),sx,'mu_r_ea')
-        mur_oaPtr = mxGetField(prhs(1),sx,'mu_r_oa')
-        MremPtr = mxGetField(prhs(1),sx,'Mrem')
-        tileTypePtr = mxGetField(prhs(1),sx,'tileType')
-        offsetPtr = mxGetField(prhs(1),sx,'offset')
-        rotAnglesPtr = mxGetField(prhs(1),sx,'rotAngles')
-        rectDimsPtr =  mxGetField(prhs(1),sx,'abc')
-      
-        call mxCopyPtrToReal8(mxGetPr(r0Ptr), cylTile%r0, 1 )
-        call mxCopyPtrToReal8(mxGetPr(theta0Ptr), cylTile%theta0, 1 )
-        call mxCopyPtrToReal8(mxGetPr(z0Ptr), cylTile%z0, 1 )
-        call mxCopyPtrToReal8(mxGetPr(drPtr), cylTile%dr, 1 )
-        call mxCopyPtrToReal8(mxGetPr(dthetaPtr), cylTile%dtheta, 1 )
-        call mxCopyPtrToReal8(mxGetPr(dzPtr), cylTile%dz, 1 )
-        call mxCopyPtrToReal8(mxGetPr(MPtr), cylTile%M, 3 )
-        call mxCopyPtrToReal8(mxGetPr(u_eaPtr), cylTile%u_ea, 3 )
-        call mxCopyPtrToReal8(mxGetPr(u_oa1Ptr), cylTile%u_oa1, 3 )
-        call mxCopyPtrToReal8(mxGetPr(u_oa2Ptr), cylTile%u_oa2, 3 )
-        call mxCopyPtrToReal8(mxGetPr(mur_eaPtr), cylTile%mu_r_ea, 1 )
-        call mxCopyPtrToReal8(mxGetPr(mur_oaPtr), cylTile%mu_r_oa, 1 )
-        call mxCopyPtrToReal8(mxGetPr(MremPtr), cylTile%Mrem, 1 )
-        call mxCopyPtrToInteger4(mxGetPr(tileTypePtr), cylTile%tileType, 1 )
-        call mxCopyPtrToReal8(mxGetPr(offsetPtr), cylTile%offset, 3 )
-        call mxCopyPtrToReal8(mxGetPr(rotAnglesPtr), cylTile%rotAngles, 3 )
-        call mxCopyPtrToReal8(mxGetPr(rectDimsPtr), rectDims, 3 )
-        cylTile%a = rectDims(1)
-        cylTile%b = rectDims(2)
-        cylTile%c = rectDims(3)
+      !::Copy the input parameters      
+      call loadMagTile( prhs(1), cylTile, 1 )
       
       !::copy the number of points where the tensor field is required
       call mxCopyPtrToInteger4(mxGetPr(prhs(3)), n_ele,1)
@@ -115,7 +60,7 @@
       !::do the calculation
       N(:,:,:) = 0
       H(:,:) = 0      
-      call getFieldFromCylTile( cylTile, H, pts, n_ele, N )
+      call getFieldFromCylTile( cylTile(1), H, pts, n_ele, N )
       
       !::Load the result back to Matlab
       
