495c2574bd2aba06f429906eae78f6822819e820
spol <spol@dtu.dk>
2019-08-13 13:49:15 +0200

    Tiny examples for publication
    Four prisims planar
    Halbach structure
    Tiles in 3D grid

--- python/examples/four_prism_tiles.py
+++ python/examples/four_prism_tiles.py
@@ -1,13 +1,12 @@
 import os
 import sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../lib_mag')
 import numpy as np
 import math
 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../source')
 import MagTense
 import MagTenseStandalone
 import util_plot
-import MagTenseSource
 
 def main():
     # Defining grid and positions, angles of magnets
@@ -16,10 +15,11 @@ def main():
     filled_positions = [[2, 4, 0], [4, 2, 0], [4, 6, 0], [6, 4, 0]]
     mag_angles = [0, math.pi, math.pi, 0]
 
-    # Standard parameters in settings: max_error=0.00001, max_it=500, iterate_solution=1, return_field=1
-    # Optional parameters for setup: param_settings, n_magnets, area, eval_points, eval_mode 
+    # Optional parameters for setup: n_magnets, filled_positions, mag_angles, eval_points, eval_mode, B_rem
     (tiles, points, grid) = MagTense.setup(places, area, filled_positions=filled_positions, mag_angles=mag_angles)
+    print(tiles)
 
+    # Standard parameters in settings: max_error=0.00001, max_it=500
     iterated_tiles = MagTense.iterate_magnetization(tiles)
     N = MagTense.get_N_tensor(iterated_tiles,points)
     H = MagTense.get_H_field(iterated_tiles,points,N)
@@ -27,8 +27,8 @@ def main():
 
     # All steps in one command - iterate_solution = True, return_field = True
     # N is not reused for calculating the H field
-    # (updated_tiles, solution) = MagTense.run_simulation(tiles, points, grid=grid, plot=False)
-    # util_plot.create_plot(updated_tiles, solution, grid=grid)
+    # (updated_tiles, H) = MagTense.run_simulation(tiles, points, grid=grid, plot=False)
+    # util_plot.create_plot(updated_tiles, points, H, grid=grid)
 
     # Testing the standalone version
     # (tiles, points, grid) = MagTenseStandalone.setup(places, area, filled_positions=filled_positions, mag_angles=mag_angles)
