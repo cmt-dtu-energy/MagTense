ea3ea0256b43f413207ede28f9aec73117944a42
spol <spol@dtu.dk>
2019-08-27 14:17:56 +0200

    Update on validation experiment of cylindrical tiles
    Plotting of cylindrical tiles implemented
    Curved surfaces are missing

--- python/examples/validation_cylinder.py
+++ python/examples/validation_cylinder.py
@@ -19,19 +19,20 @@ def main():
 
     # Define validation tile
     tile_val = MagTense.Tiles(1)
-    tile_val.set_center_pos([[0.3, math.pi/2, 0.25]])
-    tile_val.set_dev_center([0.2, math.pi/4, 0.4])
-    tile_val.set_offset_i([0.5, 0.5, 0.5],0)
+    tile_val.set_center_pos([[0.3, math.pi/2, 0.5]])
+    tile_val.set_dev_center([0.3, math.pi/4, 0.1])
+    tile_val.set_offset_i([0.8, -0.1, 0.3],0)
+    # tile_val.set_rotation_i([-math.pi/6, math.pi/5, math.pi/2],0)
     tile_val.set_rotation_i([0, 0, 0],0)
     tile_val.set_tile_type(1)
     tile_val.set_remanence(B_rem / (4*math.pi*1e-7))
-    tile_val.set_mag_angle([[math.pi/8, math.pi/2]])
+    tile_val.set_mag_angle([[math.pi/4, -math.pi/2]])
     tile_val.set_color([[1, 0, 0]])
 
     # Standard parameters in settings: max_error=0.00001, max_it=500
     iterated_tiles = MagTense.iterate_magnetization(tile_val)
     N = MagTense.get_N_tensor(iterated_tiles,points)
     H = MagTense.get_H_field(iterated_tiles,points,N)
-    util_plot.create_plot(None, points, H, grid=grid)
+    util_plot.create_plot(tile_val, points, H, grid=grid)
 
 main()
