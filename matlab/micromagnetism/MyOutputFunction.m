f52d890473c5e851b8e0f3f9fc3cfcfca4d08f83
Rasmus <rabj@dtu.dk>
2019-11-13 07:47:42 +0100

    Updated Matlab model and added a Fortran version for standard problem 4
    The matlab and fortran versions are now directly compared by running the Standard_problem_4.m example

---
+++ matlab/micromagnetism/MyOutputFunction.m
@@ -0,0 +1,29 @@
+function status = MyOutputFunction(t,SigmaSol,flag,UseImplicitSolver,dSigmaMagTol)
+if isempty(flag) 
+    if UseImplicitSolver
+        global TheData
+        LastP = TheData.p ;
+        disp([num2str(t(end)),'  Last P : ',num2str(LastP)])
+
+        if (LastP > 0)
+            status = 0 ; % was 1
+        else
+            status = 0 ; 
+        end
+    else
+        try
+            global TheData
+            LastRMS = TheData.dSigmaRMS ;
+        catch
+            LastRMS = inf ;
+        end
+
+        disp(['Log10dS ',num2str(log10(LastRMS))]) ;
+        if (log10(LastRMS) < dSigmaMagTol)
+            status = 1 ; % was 1
+        else
+            status = 0 ; 
+        end
+    end
+end
+end
