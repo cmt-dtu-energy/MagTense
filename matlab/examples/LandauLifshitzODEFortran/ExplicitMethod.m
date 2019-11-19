cd20c63572c864a2bf7fb095a1d4aba551bcf7ec
Kaspar <kaki@dtu.dk>
2019-11-19 15:04:56 +0100

    Building up a test of the explicit solver

---
+++ matlab/examples/LandauLifshitzODEFortran/ExplicitMethod.m
@@ -0,0 +1,52 @@
+clearvars
+close all
+
+addpath('../../MEX_files');
+addpath('../util');
+tic
+%test the Fortran implementation of the LL-ODE solver
+%get the default problem
+%takes the size of the grid as arguments (nx,ny,nz) and a function handle
+%that produces the desired field (if not present zero applied field is
+%inferred)
+problem = DefaultMicroMagProblem(36,9,1);
+
+problem.alpha = -4.42e-6;
+problem.gamma = 0;
+
+%initial magnetization
+problem.m0(:) = 1/sqrt(3);
+
+
+%time grid on which to solve the problem
+problem = problem.setTime( linspace(0,50,20) );
+HystDir = -[1,1,1] ;
+problem.solver = getMicroMagSolver( 'Explicit' );
+
+t_H_final = 100;
+%time-dependent applied field
+HextFct = @(t) (t_H_final-t)' .* HystDir .* (t<t_H_final)'./t_H_final;
+
+problem = problem.setHext( HextFct );
+
+solution = struct();
+%convert the class obj to a struct so it can be loaded into fortran
+prob_struct = struct(problem);
+
+
+solution = MagTenseLandauLifshitzSolver_mex( prob_struct, solution );
+
+%find the mean value of the last nt time-evolution points
+nt = 10;
+M_mean = mean(solution.M(end-nt:end,:,:,:),1);
+
+%find the rms value of the last nt values
+
+rms = sqrt( sum(( M_mean(1,:,:,:)-solution.M(end-nt:end,:,:,:) ).^2,1) );
+figure
+hold on
+col = lines(3);
+plot(squeeze(mean(rms(1,:,:,1))),'o','color',col(1,:));
+plot(squeeze(mean(rms(1,:,:,2))),'o','color',col(2,:));
+plot(squeeze(mean(rms(1,:,:,3))),'o','color',col(3,:));
+
--- matlab/examples/LandauLifshitzODEFortran/Standard_problem_4.m
+++ matlab/examples/LandauLifshitzODEFortran/Standard_problem_4.m
@@ -11,7 +11,7 @@ tic
 %takes the size of the grid as arguments (nx,ny,nz) and a function handle
 %that produces the desired field (if not present zero applied field is
 %inferred)
-problem = DefaultMicroMagProblem(36,9,1);
+problem = DefaultMicroMagProblem(72,18,2);
 
 problem.alpha = -4.42e-6;
 problem.gamma = 0;
@@ -39,7 +39,7 @@ solution = MagTenseLandauLifshitzSolver_mex( prob_struct, solution );
 
 %setup problem for the time-dependent solver
 
-problem = DefaultMicroMagProblem(36,9,1);
+problem = DefaultMicroMagProblem(72,18,2);
 % problem.gamma = -2.21e5;
 % problem.alpha = -4.42e3/problem.Ms;
 problem.alpha = -4.42e-6 ;
@@ -47,6 +47,7 @@ problem.gamma = -2.21e-4 ;
 problem.dem_thres = 1e-6;
 % problem = problem.setTime( linspace(0,1e15,100) ); % 1e16
 problem = problem.setTime( linspace(0,1,200) ); %
+
 %field 1
 HystDir = -[-24.6,4.3,0]/1000 ;
 %field 2
