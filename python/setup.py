ddc05bf9573e8231028f1db15417b35cce7df217
spol <spol@dtu.dk>
2022-05-05 12:13:41 +0200

    [python] Ready for distribution via PyPi

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as fh:
 
 setup(
     name='magtense',
-    version='0.1.0',
+    version='1.0.1',
     description="MagTense - a micromagnetism and magnetostatic framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -13,10 +13,12 @@ setup(
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
     ],
-    url="www.magtense.org",
-    author="cmt-dtu-energy",
+    url="https://www.magtense.org/",
+    author="Rasmus Bjørk",
+    author_email="rabj@dtu.dk",
     license="GPL 3.0",
     packages=find_packages(include=['magtense', 'magtense.*']),
     package_data={'magtense': ['utils/data/*.csv']},
