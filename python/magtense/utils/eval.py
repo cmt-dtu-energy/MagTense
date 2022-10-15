b9b376afca086822dd18881f7dddcd235b97a4a2
spol <spol@dtu.dk>
2022-10-15 02:11:19 +0200

    [python] Streamlining of validation scripts

--- python/magtense/utils/eval.py
+++ python/magtense/utils/eval.py
@@ -1,51 +1,93 @@
-import os
 import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import List, Optional
+from pathlib import Path
+
+from magtense.magstatics import Tiles, run_simulation
+
 
 def get_average_magnetic_flux(H):
-    norm = get_norm_magnetic_flux(H)
+    mu0 = 4 * np.pi * 1e-7
+    norm = [np.linalg.norm(H_point) * mu0 for H_point in H]
     return sum(norm) / len(norm)
 
+
 def get_p2p(H):
-    norm = get_norm_magnetic_flux(H)
+    mu0 = 4 * np.pi * 1e-7
+    norm = [np.linalg.norm(H_point) * mu0 for H_point in H]
     return max(norm) - min(norm)
 
-def get_norm_magnetic_flux(H):
-    return [np.linalg.norm(H_point) * 4 * np.pi * 1e-7 for H_point in H]
 
-# Load reference points from COMSOL calculation
-def load_COMSOL_eval(file_name, eval_offset, COMSOL_eval_path, model_offset=[0,0,0], pts_special=None, unit='A/m'):
-    with open(os.path.join(COMSOL_eval_path, file_name), "r") as file:
-                T = file.readlines()[8:]
+def load_COMSOL(
+    fname: str,
+    eval_offset: List,
+    COMSOL_eval_path: Path, 
+    model_offset: List,
+    unit: str,
+    pts_special: Optional[np.ndarray] = None,
+    ) -> tuple[np.ndarray, np.ndarray]:
+    '''
+    Load reference points from COMSOL calculation
+    '''
+    with open(Path(COMSOL_eval_path, fname), "r") as file:
+        T = file.readlines()[8:]
 
     T_split = np.asarray([line.split() for line in T], dtype=np.float64)
     H_norm_COMSOL = T_split[:,1]
-
-    # Preparing points along given axis
-    if pts_special is None:
-        pts_coor = T_split[:,0]
-    else:
-        pts_coor = pts_special
+    if unit == 'T': H_norm_COMSOL *= 4 * np.pi * 1e-7
+    pts_coor = T_split[:,0] if pts_special is None else pts_special
     struc = np.ones(len(pts_coor))
-    if file_name[-5] == 'x':
-        pts = np.c_[pts_coor-model_offset[0], struc*eval_offset[1], struc*eval_offset[2]]
-    elif file_name[-5] == 'y':
-        pts = np.c_[struc*eval_offset[0], pts_coor-model_offset[1], struc*eval_offset[2]]
-    elif file_name[-5] == 'z':
-        pts = np.c_[struc*eval_offset[0], struc*eval_offset[1], pts_coor-model_offset[2]]
 
-    if unit == 'T':
-        H_norm_COMSOL = H_norm_COMSOL*(4*np.pi*1e-7)
+    if fname[-5] == 'x':
+        pts = np.c_[pts_coor - model_offset[0],
+                    struc * eval_offset[1],
+                    struc * eval_offset[2]]
+    elif fname[-5] == 'y':
+        pts = np.c_[struc * eval_offset[0],
+                    pts_coor - model_offset[1],
+                    struc * eval_offset[2]]
+    elif fname[-5] == 'z':
+        pts = np.c_[struc * eval_offset[0],
+                    struc * eval_offset[1],
+                    pts_coor - model_offset[2]]
 
-    return (pts, H_norm_COMSOL)
+    return pts, H_norm_COMSOL
 
-# Adding subplot to figure
-def add_subplot(ax, x, x_label, y_MagTense, y_COMSOL, plot_COMSOL=True, plot_error=False):    
-    ax.plot(x, y_MagTense, 'r*', label='MagTense')
-    if plot_COMSOL is True:
-        ax.plot(x, y_COMSOL, 'bx', label='COMSOL')
-    if plot_error is True:
-        error = abs(y_COMSOL - y_MagTense)
-        ax.plot(x, error, 'g', label= 'Error')
-    ax.legend()
-    ax.set_xlabel(x_label)
-    ax.set_ylabel('H_norm')
+
+def validation(
+    shape: str,
+    tile: Tiles,
+    offset: List,
+    model_offset: List = [0, 0, 0],
+    plot_COMSOL: bool = True,
+    plot_error: bool = False,
+    unit: str = 'A/m'
+    ) -> None:
+    mu0 = 4 * np.pi * 1e-7
+    prefix = 'py_' if 'spher' in shape else ''
+    suffix = '_prolate' if shape == 'spheroid' else ''
+    COMSOL_eval_path = Path(__file__).parent.absolute() / '..' / '..' / '..' / \
+        'documentation' / 'examples_FEM_validation' / f'Validation_{shape}'
+
+    fig, ax = plt.subplots(1,3)
+    fig.suptitle(f'{shape} - MagTensePy vs. COMSOL')
+
+    for i, coord in enumerate(['x', 'y', 'z']):
+        fname = f'{prefix}Validation_{shape}{suffix}_normH_{coord}.txt'
+        pts, H_n_COMSOL = load_COMSOL(fname, offset, COMSOL_eval_path, model_offset, unit)
+        _, H_mt = run_simulation(tile, pts)
+        H_n_mt = [np.linalg.norm(H_point) * mu0 for H_point in H_mt]
+
+        ax[i].plot(pts[:,i], H_n_mt, 'r*', label='MagTense')
+        if plot_COMSOL: ax[i].plot(pts[:,i], H_n_COMSOL, 'bx', label='COMSOL')
+
+        if plot_error:
+            error = abs(H_n_COMSOL - H_n_mt)
+            ax[i].plot(pts[:,i], error, 'g', label= 'Error')
+
+        ax[i].legend()
+        ax[i].set_xlabel(f'{coord}_axis')
+        ax[i].set_ylabel('H_norm')
+
+    plt.show()
