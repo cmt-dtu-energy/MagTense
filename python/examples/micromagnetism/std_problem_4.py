2a0bdd7ae5aa84fc71fa3b881e31bddcb447349b
spol <spol@dtu.dk>
2022-10-24 10:57:50 +0200

    [python] Getter / setter func for MicromagProblem

--- python/examples/micromagnetism/std_problem_4.py
+++ python/examples/micromagnetism/std_problem_4.py
@@ -10,30 +10,26 @@ def std_prob_4(res=[64,16,1], NIST_field=1, cuda=False, show=True):
     grid_L = [500e-9, 125e-9, 3e-9]
 
     ### Magnetization to s-state
-    problem_ini = MicromagProblem(res, grid_L, alpha=4.42e3, cuda=cuda)    
-    problem_ini.m0[:] = 1 / np.sqrt(3)
+    problem_ini = MicromagProblem(res, grid_L, m0=1/np.sqrt(3), alpha=4.42e3, cuda=cuda)
 
-    Hyst_dir = 1 / mu0 *  np.array([1, 1, 1])
+    Hyst_dir = np.array([1, 1, 1]) / mu0
     HextFct = lambda t: np.expand_dims(1e-9 - t, 0).T * Hyst_dir * np.expand_dims(t < 1e-9, 0).T
 
     _, M_out, pts, _, _, _, _ = problem_ini.run_simulation(100e-9, 200, HextFct, 2000)
     M_sq_ini = np.squeeze(M_out, axis=2)
 
     ### Time-dependent solver
-    problem_dym = MicromagProblem(res, grid_L, alpha=4.42e3, gamma=2.21e5, cuda=cuda)
-    # s-state as initial magnetization
-    problem_dym.m0 = M_out[-1, :, 0, :]
+    problem_dym = MicromagProblem(res, grid_L, m0=M_sq_ini[-1], alpha=4.42e3, gamma=2.21e5, cuda=cuda)
 
-    # Two applied fields of std problem 4
+    # Two applied external fields of std problem 4
     if NIST_field == 1:
-        ext_field = [-24.6, 4.3, 0]
+        ext_field = np.array([-24.6, 4.3, 0]) / 1000 / mu0
     elif NIST_field == 2:
-        ext_field = [-35.5, -6.3, 0]
+        ext_field = np.array([-35.5, -6.3, 0]) / 1000 / mu0
     else:
         raise NotImplementedError()
 
-    Hyst_dir = (np.array(ext_field) / 1000) / mu0
-    HextFct = lambda t: np.expand_dims(t > -1, 0).T * Hyst_dir
+    HextFct = lambda t: np.expand_dims(t > -1, 0).T * ext_field
     t_dym, M_out, pts, _, _, _, _ = problem_dym.run_simulation(1e-9, 200, HextFct, 2000)
     M_sq_dym = np.squeeze(M_out, axis=2)
 
