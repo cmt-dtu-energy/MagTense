c12741f567c3bf193fb76b83663e9535fe31e460
Rasmus <rabj@dtu.dk>
2022-11-09 12:36:09 +0100

    Updated documentation

--- matlab/examples/Micromagnetism/mumag_micromag_Std_problem_4/Standard_problem_4_unstructured_cart.m
+++ matlab/examples/Micromagnetism/mumag_micromag_Std_problem_4/Standard_problem_4_unstructured_cart.m
@@ -1,33 +1,80 @@
-function [elapsedTime_part1,elapsedTime_part2,problem_ini,solution_ini,problem_dym,solution_dym,mesh,GridInfo] = Standard_problem_4_unstructured_cart( NIST_field, n, L, mesh_scale )
+function [elapsedTime_part1,elapsedTime_part2,problem_ini,solution_ini,problem_dym,solution_dym,mesh,GridInfo] = Standard_problem_4_unstructured_cart( NIST_field, options )
+%STANDARD_PROBLEM_4_UNSTRUCTURED_CART 
+%A function script to setup and simulate mumag standard problem 4 with an unstructured cartesian mesh loaded from a file
+%
+%Syntax:
+%------
+%   Standard_problem_4_unstructured_cart()
+%   [elapsedTime_part1,elapsedTime_part2,problem_ini,solution_ini,problem_dym,solution_dym,mesh,GridInfo] = Standard_problem_4( NIST_field, options)
+%
+%Description of syntax:
+%------
+%   Standard_problem_4_unstructured_cart() 
+%       Uses the default parameters to solve mumag problem 4 and displays the results on screen
+%
+%   Standard_problem_4_unstructured_cart( NIST_field, options)
+%       Takes 1 input argument which specifies the applied field. Additional options can also be specified
+%
+%   [elapsedTime_part1,elapsedTime_part2,problem_ini,solution_ini,problem_dym,solution_dym,mesh,GridInfo] = Standard_problem_4_unstructured_cart( NIST_field, options)
+%       As above but also returns the computation times, the problem setup file and the solution for both the initial and dynamical part of the problem as well as the mesh information
+%
+%Input arguments:
+%------
+%   NIST_field : Either 1 or 2
+%       Determines if the first or second applied field specified in the mumag problem description is used (Default value is 1)
+%
+%Options:
+%-------
+%   use_CUDA : Interpreted as a logical - Default is true
+%       Determines if CUDA is used for the computation.
+%
+%   ShowTheResult : Interpreted as a logical - Default is true
+%       Determines if the results are plotted or not.
+%
+%Output arguments:
+%-------
+%   elapsedTime_part1 : Double
+%      The time takes to compute the initial part of the problem
+%
+%   elapsedTime_part2 : Double
+%      The time takes to compute the dynamic part of the problem
+%
+%   problem_ini : Struct
+%      A struct containing the MagTense problem setup for the initial part of the mumag standard problem 4
+% 
+%   solution_ini : Struct
+%      A struct containing the MagTense solution for the initial part of the mumag standard problem 4
+%
+%   problem_dym : Struct
+%      A struct containing the MagTense problem setup for the dynamic part of the mumag standard problem 4
+% 
+%   solution_dym : Struct
+%      A struct containing the MagTense solution for the dynamic part of the mumag standard problem 4
+%
+%   mesh : Struct
+%      Contains information on the mesh used
+%
+%   GridInfo : Struct
+%      Contains information on the mesh used
+%
+%Detailed description:
+%-------
+%   The script setups up and runs the mumag standard problem 4 for a prismal mesh.
+%
+%Version: 1.0.0
+%Author:  Rasmus Bjørk
+%Date:    2022.11.09
+%
+%See also: Standard_problem_4
 
-clearvars -except NIST_field resolution use_CUDA SaveTheResult ShowTheResult weight_direct n L mesh_scale
-% close all
-
-%--- Use either field 1 or field 2 from the NIST example
-if ~exist('NIST_field','var')
-    NIST_field = 1;
-end
-if ~exist('resolution','var')
-    resolution = [1*36,1*9,1];
-end
-if ~exist('use_CUDA','var')
-    use_CUDA = true;
-end
-if ~exist('ShowTheResult','var')
-    ShowTheResult = 1;
-end
-if ~exist('SaveTheResult','var')
-    SaveTheResult = 0;
+arguments
+    NIST_field (1,1) {mustBeInteger} = 1                %--- Use either field 1 or field 2 from the mumag example
+    options.use_CUDA {mustBeNumericOrLogical} = true 
+    options.ShowTheResult {mustBeNumericOrLogical} = true
+    options.use_CVODE {mustBeNumericOrLogical} = false;
 end
 
-if use_CUDA
-    dir_m = 'GPU';
-else
-    dir_m = 'CPU';
-end
-use_CVODE = false;
-
-if (ShowTheResult)
+if (options.ShowTheResult)
     figure1= figure('PaperType','A4','Visible','on','PaperPositionMode', 'auto'); fig1 = axes('Parent',figure1,'Layer','top','FontSize',16); hold on; grid on; box on
 end
 mu0 = 4*pi*1e-7;
@@ -67,8 +114,8 @@ problem_ini = problem_ini.setExchangeMatrixSparse( InteractionMatrices.A2 );
 
 %% Setup the problem for the initial configuration
 problem_ini = problem_ini.setMicroMagDemagApproximation('none');
-problem_ini = problem_ini.setUseCuda( use_CUDA );
-problem_ini = problem_ini.setUseCVODE( use_CVODE );
+problem_ini = problem_ini.setUseCuda( options.use_CUDA );
+problem_ini = problem_ini.setUseCVODE( options.use_CVODE );
 
 problem_ini.alpha = 4.42e3;
 problem_ini.gamma = 0;
@@ -95,7 +142,7 @@ tic
 solution_ini = problem_ini.MagTenseLandauLifshitzSolver_mex( prob_struct, solution_ini );
 % [Mx,My,Mz,mx,my,mz] = computeMagneticMomentGeneralMesh(solution_ini.M,GridInfo.Volumes) ;
 elapsedTime_part1 = toc
-if (ShowTheResult)
+if (options.ShowTheResult)
     figure; M_end = squeeze(solution_ini.M(end,:,:)); quiver(solution_ini.pts(:,1),solution_ini.pts(:,2),M_end(:,1),M_end(:,2)); axis equal; title('Starting state - Fortran')
 end
 
@@ -117,8 +164,8 @@ problem_dym.exch_col = problem_ini.exch_col;
 
 problem_dym.alpha = 4.42e3 ;
 problem_dym.gamma = 2.21e5 ;
-problem_dym = problem_dym.setUseCuda( use_CUDA );
-problem_dym = problem_dym.setUseCVODE( use_CVODE );
+problem_dym = problem_dym.setUseCuda( options.use_CUDA );
+problem_dym = problem_dym.setUseCVODE( options.use_CVODE );
 problem_dym = problem_dym.setMicroMagDemagApproximation('none');
 problem_dym = problem_dym.setTime( linspace(0,1e-9,200) );
 problem_dym.setTimeDis = int32(10);
@@ -146,7 +193,7 @@ solution_dym = problem_dym.MagTenseLandauLifshitzSolver_mex( prob_struct, soluti
 elapsedTime_part2 = toc
 
 [Mx,My,Mz,mx,my,mz] = computeMagneticMomentGeneralMesh(solution_dym.M,GridInfo.Volumes) ;
-if (ShowTheResult)
+if (options.ShowTheResult)
     plot(fig1,solution_dym.t,Mx,'rx');
     plot(fig1,solution_dym.t,My,'gx');
     plot(fig1,solution_dym.t,Mz,'bx');
@@ -156,7 +203,7 @@ end
 %% --------------------------------------------------------------------  mumag -----------------------------------------------------------
 %% --------------------------------------------------------------------------------------------------------------------------------------
 %% Compare with published solutions available from mumag webpage
-if (ShowTheResult)
+if (options.ShowTheResult)
     t=linspace(0,1,1000);
     data = load(['Published_solutions_field' num2str(NIST_field)]);
     mean_avg=data.mean_avg; err=data.err;
