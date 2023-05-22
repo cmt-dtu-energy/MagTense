be11e8e57cf2ff4353288ba584d3f62fdba43cc2
Berian James <1518788+berianjames@users.noreply.github.com>
2023-05-22 12:04:36 +0200

    gotta cmp logicals with .eqv. lol

--- source/NumericalIntegration/NumericalIntegration/IOgeneral.f90
+++ source/NumericalIntegration/NumericalIntegration/IOgeneral.f90
@@ -14,13 +14,13 @@ module IO_GENERAL
     !> Rasmus Bjørk, rabj@dtu.dk, August 2022
     !> Callback to display message to the GUI launching the simulation
     !> @params[in] mess The message to display to the GUI
-    !>----------------------------------------    
-    
+    !>----------------------------------------
+
     subroutine displayGUIMessage( mess )
         character(*),intent(in) :: mess
         logical :: ex
 
-        
+
         !! test if we are inside Matlab or called from a stand-alone. The simple test
         !! is whether io.txt exists in the current path (false for Matlab, true for stand-alone)
         !! nothing bad should happen if in fact we are called from ML but the file somehow
@@ -28,36 +28,36 @@ module IO_GENERAL
 
         inquire( file='io.txt', EXIST=ex )
 
-        if ( ex .eq. .true. ) then
+        if ( ex .eqv. .true. ) then
             write(*,*) mess
         endif
-        
-#if USE_MATLAB    
+
+#if USE_MATLAB
         call displayMatlabMessage( mess )
-#endif 
-    
+#endif
+
     end subroutine displayGUIMessage
 
-    
+
     subroutine displayGUIProgressMessage( mess, prog )
         character(*),intent(in) :: mess
         integer,intent(in) :: prog
         character*(100) :: prog_str
-        
+
         if (prog .lt. 0) then
             call displayGUIMessage( trim(mess) )
         else
-            write(prog_str,'(A50, I4.0)') mess, prog           
+            write(prog_str,'(A50, I4.0)') mess, prog
             call displayGUIMessage( trim(prog_str) )
         endif
 
     end subroutine displayGUIProgressMessage
-    
+
     !--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     !---------------------------------------------------------------- Message Matlab interface ---------------------------------------------------------------------------------
     !--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#if USE_MATLAB  
+#if USE_MATLAB
 
     !! Routine for displaying progress within Matlab
     subroutine displayMatlabMessage( mess )
@@ -66,18 +66,18 @@ module IO_GENERAL
         integer :: mexCallMATLAB, nlhs_cb, nrhs_cb, tmp
         mwPointer plhs_cb(1), prhs_cb(1),mxCreateString
         character*(4) functionName_cb
-        
+
         functionName_cb = "disp"
         nlhs_cb = 0
         nrhs_cb = 1
-        
+
         prhs_cb(1) = mxCreateString(mess)
-        
+
         tmp = mexCallMATLAB(nlhs_cb, plhs_cb, nrhs_cb, prhs_cb, "disp")
-        
+
 
     end subroutine displayMatlabMessage
-   
+
 #endif
-    
+
 end module IO_GENERAL
