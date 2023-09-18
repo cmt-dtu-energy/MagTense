613a046e2ef8346acb52cbc8afa1d6cbc2bd0220
spol <spol@dtu.dk>
2023-09-19 00:17:48 +0200

    Adjust automated packaging to windows

--- python/scripts/dist_conda.py
+++ python/scripts/dist_conda.py
@@ -1,25 +1,48 @@
 import os
 import platform
 import subprocess
+import shutil
 
 
-def main(mt_version = '2.2.1', py_versions = ['3.10', '3.11']):
+def main(mt_version = '2.2.1', py_versions = ['3.11']):
     if platform.system() == 'Windows':
         arch = "win-64"
     else:
         arch = "linux-64"
 
-    for py in py_versions:
-        for cuda in ["cpu", "cuda12"]:
-            if cuda == "cpu":
-                s_make = '1,4c\\==========================\\n# Compiler names and flags\\==========================\\nUSE_CUDA = 0'
-            else:
-                s_make = '1,4c\\==========================\\n# Compiler names and flags\\n==========================\\nUSE_CUDA = 1'
-            
-            s_meta = f'1,3c\\{{% set version = "{mt_version}" %}}\\n{{% set py_version = "{py}" %}}\\n{{% set cuda_version = "{cuda}" %}}'
+    s_0 = "#=========================="
+    s_1 = "# Compiler names and flags"
 
-            subprocess.run(["sed", "-i", s_make, "src/magtense/lib/Makefile"])
-            subprocess.run(["sed", "-i", s_meta, "../.conda-recipe/meta.yaml"])
+    for py in py_versions:
+        for cuda in ["cuda12"]:
+            if cuda == "cpu":
+                s_2 = "USE_CUDA = 0"
+            else:
+                s_2 = "USE_CUDA = 1"
+        
+            with open("src/magtense/lib/Makefile", 'r') as source_file:
+                lines_to_append = source_file.readlines()[4:]
+
+            with open("scripts/temp.txt", "w") as file:
+                file.write(s_0 + '\n')
+                file.write(s_1 + '\n')
+                file.write(s_0 + '\n')
+                file.write(s_2 + '\n')
+                file.writelines(lines_to_append)
+
+            shutil.move("scripts/temp.txt", "src/magtense/lib/Makefile")
+            
+            with open("../.conda-recipe/meta.yaml", 'r') as source_file:
+                lines_to_append = source_file.readlines()[3:]
+
+            with open("scripts/temp.txt", "w") as file:
+                file.write(f'{{% set version = "{mt_version}" %}}' + '\n')
+                file.write(f'{{% set py_version = "{py}" %}}' + '\n')
+                file.write(f'{{% set cuda_version = "{cuda}" %}}' + '\n')
+                file.writelines(lines_to_append)
+
+            shutil.move("scripts/temp.txt", "../.conda-recipe/meta.yaml")
+
             subprocess.run(["conda-build", "../.conda-recipe"])
 
             upload_command = [
