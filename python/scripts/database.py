05c811f3e8a01223b8c1039ec565dbf6a301f7e9
spol <spol@dtu.dk>
2023-11-01 21:12:50 +0100

    Script for producing 2D fields

--- python/scripts/database.py
+++ python/scripts/database.py
@@ -383,6 +383,124 @@ def db_magfield(
     db.close()
 
 
+def db_magfield_symm(
+    datapath: Path,
+    n_samples: int,
+    res: List[int],
+    spots: List = [10, 10, 5],
+    area: List = [1, 1, 0.5],
+    gap: float = 0.05,
+    seed: int = 0,
+    intv: Optional[List] = None,
+    name: str = 'magfield_symm',
+    empty: bool = False
+) -> None:
+    """ 
+    Generate 3-D magnetic fields of experimental setup.
+
+    Args:
+        filepath: Indicates where to store the data sample.
+        n_samples: Size of database.
+        res: Resolution of magnetic field.
+        spots: Available positions in setup.
+        area: Size of setup.
+        gap: Gap between measurement area and surrounding magnets.
+        seed: Seed for random number generator of matrices.
+        intv: Data range to iterate over.
+        empty: If set, an empty database is created.
+    """
+    symm = True
+    fname = f'{name}_{res[0]}'
+    if intv is None: intv = [0, n_samples]
+    if not empty: fname += f'_{intv[0]}_{intv[1]}'
+    n_intv = intv[1] - intv[0]
+    
+    db = h5py.File(f'{datapath}/{fname}.h5', libver='latest', mode='w')
+    out_shape = (n_intv, 3, *res) # if not symm else (n_intv, 2, *res)
+    db.create_dataset('field', shape=out_shape, dtype="float32")
+    db.create_dataset('scalar_potential', shape=(n_intv, 1, *res), dtype="float32")
+    if not empty: db.attrs['intv'] = intv
+
+    if symm:
+        spots[2] = 1
+        area[2] = 10
+    
+    if empty:
+        db.attrs['spots'] = spots
+        db.attrs['area'] = area
+        db.attrs['gap'] = gap
+        db.attrs['seed'] = seed
+        db.close()
+
+        return fname, n_samples
+
+    rng = np.random.default_rng(seed)
+    tile_size = np.asarray(area) / np.asarray(spots)
+
+    filled_mat = rng.integers(2, size=(n_samples, spots[0], spots[1], spots[2]))
+    mag_angle_mat = rng.random(size=(n_samples, spots[0] * spots[1] * spots[2]))
+    
+    for idx in tqdm(range(n_intv)):
+        emp_x = spots[0] // 2 - 1
+        emp_y = emp_x + 1
+        s_x = emp_x * tile_size[0]
+        s_y = emp_y * tile_size[1]
+
+        filled_pos = [[i, j, k] 
+                      for i in range(spots[0])
+                      for j in range(spots[1])
+                      for k in range(spots[2]) 
+                      if filled_mat[intv[0] + idx][i][j][k] == 1 and (i < emp_x or i > emp_y
+                        or j < emp_x or j > emp_y or k < (spots[2] // 2 - 1) or k > spots[2] // 2)]
+
+        tiles = Tiles(
+            n = len(filled_pos),
+            size = tile_size,
+            tile_type = 2,
+            M_rem = 1.2 / (4*np.pi*1e-7),
+            color = [1, 0, 0],
+            mag_angle=[[np.pi/2, 2*np.pi * mag_angle_mat[idx + intv[0], i]] for i in range(len(filled_pos))]
+        )
+
+        for i, pos in enumerate(filled_pos):
+            pos = np.asarray(pos)
+            if np.greater_equal(pos, spots).any():
+                raise ValueError(f'Desired position {pos} is not in the grid!')
+            tiles.offset = (np.around((pos + 0.5) * tile_size, decimals=9), i)
+
+        x_eval = np.linspace(s_x + gap, s_y + tile_size[0] - gap, res[0])
+        y_eval = np.linspace(s_x + gap, s_y + tile_size[1] - gap, res[1])
+
+        if len(res) == 2:
+            xv, yv = np.meshgrid(x_eval, y_eval)
+            zv = np.zeros(res[0] * res[1]) + area[2] / 2
+        
+        elif len(res) == 3:
+            # Pixel length in z-direction equal to x-direction
+            s_z = (s_y - s_x) / res[0]
+            z_eval = np.linspace(-s_z, s_z, res[2]) + area[2] / 2
+            xv, yv, zv = np.meshgrid(x_eval, y_eval, z_eval)
+        
+        else:
+            raise ValueError('Only 2-D and 3-D magnetic field can be generated!')
+        
+        pts_eval = np.hstack([xv.reshape(-1,1), yv.reshape(-1,1), zv.reshape(-1,1)])
+        devnull = open('/dev/null', 'w')
+        oldstdout_fno = os.dup(sys.stdout.fileno())
+        os.dup2(devnull.fileno(), 1)
+        _, h_out = run_simulation(tiles, pts_eval)
+        os.dup2(oldstdout_fno, 1)
+
+        # Tensor image with shape CxHxWxD [T]
+        field = h_out.reshape((*res,3))
+        field = field.transpose((3,0,1,2)) if len(res) == 3 else field.transpose((2,0,1))
+        # if symm: field = field[:2]
+
+        db['field'][idx] = field * 4 * np.pi * 1e-7
+
+    db.close()
+
+
 def db_single_magnets(
     datapath: Path,
     n_samples: int,
@@ -528,6 +646,8 @@ def create_db_mp(
         target = db_std_prob_4
     elif data == 'magfield':
         target = db_magfield
+    elif data == 'magfield_symm':
+        target = db_magfield_symm
     elif data == 'single_magnets':
         target = db_single_magnets
     else:
@@ -610,10 +730,11 @@ if __name__ == '__main__':
     # create_db_mp('std_prob_4', n_workers=1, **db_kwargs)
 
     db_kwargs = {
-        'n_samples': 1,
-        'res': [256, 256, 3],
+        'n_samples': 15,
+        'res': [256, 256],
+        'spots': [5, 5, 1],
     }
-    create_db_mp('magfield', n_workers=1, **db_kwargs)
+    create_db_mp('magfield_symm', n_workers=15, **db_kwargs)
 
     # db_kwargs = {
     #     'n_samples': 10,
