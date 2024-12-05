2a26fad9bdbb06e8f89e71f7920ce6b0fe0e3012
spol <spol@dtu.dk>
2024-12-05 13:03:41 +0100

    Successful distribution via conda for unix

--- python/scripts/dist_conda.py
+++ python/scripts/dist_conda.py
@@ -4,15 +4,15 @@ import subprocess
 import shutil
 
 
-def main(mt_version = '2.2.1', py_versions = ['3.11']):
-    if platform.system() == 'Windows':
+def main(mt_version="2.2.1", py_versions=["3.12"]):
+    if platform.system() == "Windows":
         arch = "win-64"
     else:
         arch = "linux-64"
 
     for py in py_versions:
         for cuda in ["cuda12"]:
-            with open("src/magtense/lib/Makefile", 'r') as source_file:
+            with open("src/magtense/lib/Makefile", "r") as source_file:
                 lines_to_append = source_file.readlines()[4:]
 
             with open("scripts/temp.txt", "w") as file:
@@ -28,14 +28,14 @@ def main(mt_version = '2.2.1', py_versions = ['3.11']):
                 file.writelines(lines_to_append)
 
             shutil.move("scripts/temp.txt", "src/magtense/lib/Makefile")
-            
-            with open("../.conda-recipe/meta.yaml", 'r') as source_file:
+
+            with open("../.conda-recipe/meta.yaml", "r") as source_file:
                 lines_to_append = source_file.readlines()[3:]
 
             with open("scripts/temp.txt", "w") as file:
-                file.write(f'{{% set version = "{mt_version}" %}}' + '\n')
-                file.write(f'{{% set py_version = "{py}" %}}' + '\n')
-                file.write(f'{{% set cuda_version = "{cuda}" %}}' + '\n')
+                file.write(f'{{% set version = "{mt_version}" %}}' + "\n")
+                file.write(f'{{% set py_version = "{py}" %}}' + "\n")
+                file.write(f'{{% set cuda_version = "{cuda}" %}}' + "\n")
                 file.writelines(lines_to_append)
 
             shutil.move("scripts/temp.txt", "../.conda-recipe/meta.yaml")
@@ -47,10 +47,11 @@ def main(mt_version = '2.2.1', py_versions = ['3.11']):
                 "upload",
                 "--user",
                 "cmt-dtu-energy",
-                f"{os.environ.get('CONDA_PREFIX')}/conda-bld/{arch}/magtense-{mt_version}-py{py}_{cuda}.tar.bz2"
+                f"{os.environ.get('CONDA_PREFIX')}/conda-bld/{arch}/magtense-{mt_version}-py{py}_{cuda}.tar.bz2",
             ]
             subprocess.run(upload_command)
             subprocess.run(["conda", "build", "purge"])
 
-if __name__== '__main__':
+
+if __name__ == "__main__":
     main()
