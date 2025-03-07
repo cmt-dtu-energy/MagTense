d1faa5e05c4bd536021a2d9ccb618ebaf40a46ef
Fábio Pinto Fortkamp <fabio@fabiofortkamp.com>
2025-03-07 12:03:15 +0100

    Modify installation script to create conda environment

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Script to setup up the MagTense Development Environment
+#
+# Trying to setup a Miniconda enviroment in a macOS with AppleSilicon
+set -e
+build_dir=$HOME/build
+mkdir -p $build_dir
+
+cwd=$(pwd)
+cd "$build_dir"
+prefix="$HOME/.local/miniconda"
+mkdir -p "$prefix"
+curl -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+chmod +x ./miniconda.sh
+./miniconda.sh -bfp "$prefix"
+rm miniconda.sh
+
+export PATH="$prefix/bin:$PATH"
+source "$prefix/bin/activate"
+conda init bash
+conda create --name magtense python=3.11
+conda activate magtense
