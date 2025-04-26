f318e410b601fe800bf548036b44b5b4a883d3eb
spol <spol@dtu.dk>
2025-04-26 11:08:40 +0200

    Only main packages in requirements.txt

--- python/.build/dist_pypi.py
+++ python/.build/dist_pypi.py
@@ -1,13 +1,12 @@
+import argparse
 import itertools
 import subprocess
-
-from pathlib import Path
-import argparse
 import tomllib
-from typing import List
+from pathlib import Path
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(description="Build and distribute.")
     parser.add_argument(
         "--py_version",
@@ -31,14 +30,16 @@ def parse_args():
 
 
 def main(
-    py_versions: List[str],
-    cu_versions: List[str],
-    platforms: List[str],
-    build_tag: dict = {"cpu": 0, "cu12": 1},
-):
+    py_versions: list[str],
+    cu_versions: list[str],
+    platforms: list[str],
+    build_tag: dict | None = None,
+) -> None:
+    if build_tag is None:
+        build_tag = {"cpu": 0, "cu12": 1}
     py_folder = Path(__file__).parent.parent
     lib_folder = py_folder / "src" / "magtense" / "lib"
-    with open(py_folder / "pyproject.toml", "rb") as f:
+    with Path.open(py_folder / "pyproject.toml", "rb") as f:
         mt_version = tomllib.load(f)["project"]["version"]
 
     for platform in platforms:
@@ -47,7 +48,7 @@ def main(
         whl_arch = "win_amd64" if platform == "win" else "manylinux1_x86_64"
 
         for lib_file in lib_folder.glob(f"*.{suffix}"):
-            subprocess.run(["rm", lib_file])
+            subprocess.run(["rm", lib_file], check=False)
 
         for cuda, py in itertools.product(cu_versions, py_versions):
             py_lib = "cp" + py if platform == "win" else "cpython-" + py
@@ -56,12 +57,15 @@ def main(
                     "cp",
                     f"{py_folder}/{cuda}_libs/magtensesource.{py_lib}-{arch}.{suffix}",
                     lib_folder,
-                ]
+                ],
+                check=False,
             )
             if platform == "linux":
                 rpath = "$ORIGIN/../../../../../lib/"
                 if cuda == "cu12":
-                    rpath += ":$ORIGIN/../../nvidia/cublas/lib/:$ORIGIN/../../nvidia/cuda_runtime/lib/:$ORIGIN/../../nvidia/cusparse/lib/"
+                    rpath += ":$ORIGIN/../../nvidia/cublas/lib/"
+                    rpath += ":$ORIGIN/../../nvidia/cuda_runtime/lib/"
+                    rpath += ":$ORIGIN/../../nvidia/cusparse/lib/"
                 subprocess.run(
                     [
                         "patchelf",
@@ -69,38 +73,34 @@ def main(
                         "--set-rpath",
                         f"{rpath}",
                         f"{lib_folder}/magtensesource.{py_lib}-{arch}.{suffix}",
-                    ]
+                    ],
+                    check=False,
                 )
             subprocess.run(
                 [
                     "cp",
                     f"{py_folder}/.build/requirements-py{py[0]}-{cuda}.txt",
                     f"{py_folder}/requirements.txt",
-                ]
+                ],
+                check=False,
             )
             subprocess.run(
                 ["python", "-m", "build", "--wheel"],
                 cwd=py_folder,
+                check=False,
             )
             subprocess.run(
                 [
                     "mv",
                     f"{py_folder}/dist/magtense-{mt_version}-py{py[0]}-none-any.whl",
                     f"{py_folder}/dist/magtense-{mt_version}-{build_tag[cuda]}-py{py}-none-{whl_arch}.whl",
-                ]
+                ],
+                check=False,
             )
 
-            # if cuda == "cu12":
-            #     subprocess.run(
-            #         [
-            #             "cp",
-            #             f"{py_folder}/dist/magtense-{mt_version}-{build_tag[cuda]}-py{py}-none-{whl_arch}.whl",
-            #             f"{py_folder}/dist/magtense-{mt_version}-py{py}-none-{whl_arch}.whl",
-            #         ]
-            #     )
-
             subprocess.run(
-                ["rm", f"{lib_folder}/magtensesource.{py_lib}-{arch}.{suffix}"]
+                ["rm", f"{lib_folder}/magtensesource.{py_lib}-{arch}.{suffix}"],
+                check=False,
             )
             if Path(py_folder / "src" / "magtense.egg-info").is_dir():
                 subprocess.run(
@@ -109,7 +109,8 @@ def main(
                         "-r",
                         f"{py_folder}/src/magtense.egg-info/",
                         f"{py_folder}/build/",
-                    ]
+                    ],
+                    check=False,
                 )
 
 
